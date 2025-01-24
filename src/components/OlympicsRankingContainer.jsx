bffd24a536fe42dcbd62c19cc301e8e79d01f741
hoki9797 <hoki9797@gmail.com>
2025-01-24 20:35:19 +0900

    fix: 로컬 스토리지에서 데이터 가져오기 성능 개선

--- src/components/OlympicsRankingContainer.jsx
+++ src/components/OlympicsRankingContainer.jsx
@@ -13,7 +13,7 @@ function OlympicsRankingContainer() {
     })
 
     // 로컬 스토리지에서 가져온 값으로 초기화를 해야 렌더링 시 저장된 화면이 출력된다.
-    const [rows, setRows] = useState(getInitialData('data'))
+    const [rows, setRows] = useState(() => getInitialData('data'))
 
     // 로컬 스토리지 저장과 상태 업데이트를 한 번에 처리
     const setRowsAndStorage = setData(setRows, 'data')
